c5358b0853cc82616a63de01c188fdb626b6d677
Echavezandrea <andreaarabeechavez@gmail.com>
2026-05-15 14:16:24 +0800

    fixed products migration

--- backend/requests_app/models.py
+++ backend/requests_app/models.py
@@ -6,6 +6,7 @@ class ProductRequest(models.Model):
     date = models.DateField()
     rank = models.CharField(max_length=20)
     cutoff = models.CharField(max_length=20)
+    products = models.TextField()
 
     def __str__(self):
         return self.name
