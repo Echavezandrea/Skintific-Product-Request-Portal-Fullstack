30fbe540ef750b9868d727800b9a69f6241c3d49
Echavezandrea <andreaarabeechavez@gmail.com>
2026-05-15 12:35:30 +0800

    connected deployed backend

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -53,7 +53,7 @@ function ProductPage({ user }) {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/submit/", {
+      const response = await fetch("https://skintific-product-request.onrender.com/api/submit/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
